b625de652875864bdfb4a6fc456a37bcb98eba92
Chance Eakin <chance.eakin@gmail.com>
2016-06-23 15:04:12 -0500

    animations working

--- js/personalstyle.js
+++ js/personalstyle.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $("li a").click(function() {
-    	var scroll = $(this).attr('href');
+        var scroll = $(this).attr('href');
         $('html, body').animate({
             scrollTop: $(scroll).offset().top
         }, 2000);
@@ -11,4 +11,21 @@ $(document).ready(function() {
             scrollTop: $("#about").offset().top
         }, 2000, 'swing');
     });
+    $("#about-title").waypoint(function() {
+        $('#about-title').removeClass('hidden');
+        $('#about-title').addClass('fadeInDown');
+    }, { offset: '70%' });
+    $("#about-left").waypoint(function() {
+        $('#about-left').removeClass('hidden');
+        $('#about-left').addClass('fadeInLeft');
+    }, { offset: '70%' });
+    $("#about-center").waypoint(function() {
+        $('#about-center').removeClass('hidden');
+        $('#about-center').addClass('fadeInUp');
+    }, { offset: '70%' });
+    $("#about-right").waypoint(function() {
+        $('#about-right').removeClass('hidden');
+        $('#about-right').addClass('fadeInRight');
+    }, { offset: '70%' });
+
 });
